4bdf3b19011e56fabb170dc9f1a365e68e3b9a28
Siva-0107 <ss9502830@gmail.com>
2024-02-14 15:14:42 +0530

    .

--- majorproject/src/Component/Assets/Login/Login.js
+++ majorproject/src/Component/Assets/Login/Login.js
@@ -5,7 +5,33 @@ import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { Link } from 'react-router-dom';
 export const Login = () => {
-  
+  const [email,setEmail]=useState(null);
+  const [password,setPassword]=useState(null);
+  const validateEmail = (email) => {
+    return String(email).toLowerCase().match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+};
+const loginUser = ()=>{
+  if(email && password){
+    if(!validateEmail(email)) return alert("Email Not Valid")
+    alert("Success")
+    axios.post('https:/localhost:7000/api/v1/user/login', {
+      email,
+      password
+    })
+    .then( (response)=> {
+      console.log(response);
+    })
+    .catch( (error) => {
+      console.log(error);
+    });
+
+  }
+  else{
+    alert("Enter all fields");
+  }
+}
+
+
   return (
 
    <>
@@ -20,6 +46,7 @@ export const Login = () => {
         id='mail-log'
         label='Email' 
         type='email '
+        onChange={(e)=>setEmail(e.target.value)}
         InputProps={{
           startAdornment: (
             <EmailOutlinedIcon style={{ marginRight: '8px' }} id='lock' />
@@ -31,6 +58,7 @@ export const Login = () => {
         id='pass-log'
         label='Password' 
         type='password'
+        onChange={(e)=>setPassword(e.target.value)}
         InputProps={{
           startAdornment: (
             <LockOutlinedIcon style={{ marginRight: '8px' }} id='lock' />
@@ -40,14 +68,14 @@ export const Login = () => {
       />
      <div id='log-rc-flex'> 
       <FormGroup>
-        <FormControlLabel id='chk-log' control={<Checkbox  size='xxl-small'/>} />
+        <FormControlLabel id='chk-log'  control={<Checkbox  size='xxl-small'/>} />
         <label id='rem-log'> Remember me</label>
       </FormGroup>
       <Link href='#' id='fgt'>
       Forget Password?
       </Link>
       </div>
-      <Button id='btn' variant='contained'>
+      <Button id='btn' onClick={loginUser} variant='contained'>
         Login
       </Button>
       <div id='aup-log'>
--- majorproject/src/Component/Assets/Signup/Signup.js
+++ majorproject/src/Component/Assets/Signup/Signup.js
@@ -1,79 +1,103 @@
-import React from 'react';
-import '../Signup/Signup.css'
-import {Button, TextField, Typography} from '@mui/material'
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { Link } from 'react-router-dom';
-import { useRef,useState,useEffect } from 'react';
+import React from "react";
+import "../Signup/Signup.css";
+import { Button, TextField, Typography } from "@mui/material";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { Link } from "react-router-dom";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
+import { AirportShuttle } from "@mui/icons-material";
 
-const USER_REGEX=/^[a-zA-Z] [a-zA-Z0-9-_] {3,23}$/;
-const PWD_REGEX=/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/
 const Signup = () => {
- /*  const [User,Setuser]=useState(" ");
-  const [ValidN,Setuser]=useState(" "); */
+  const [email,setEmail]=useState(null);
+  const [password,setPassword]=useState(null);
+  const [confirmPassword,setConfirmPassword]=useState(null);
+
+  const validateEmail = (email) => {
+    return String(email).toLowerCase().match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+};
+
+  const createUser = ()=>{
+    if(email && password && confirmPassword){
+      if(!validateEmail(email)) return alert("Email Not Valid")
+      if(password!=confirmPassword) return alert("Passwords did't match")
+      if(password.length < 6) return alert("Password length atleast 6 character")
+      alert("Success")
+      axios.post('https:/localhost:7000/api/v1/user/new', {
+        email,
+        password
+      })
+      .then( (response)=> {
+        console.log(response);
+      })
+      .catch( (error) => {
+        console.log(error);
+      });
+
+    }
+    else{
+      alert("Enter all fields");
+    }
+  }
+
+
 
   return (
+    <>
+      <div id="body">
+        <Typography>
+          <h2 id="h2-sign">Signup</h2>
+          <div id="underline"></div>
+        </Typography>
+        <TextField
+          id="mail-log"
+          label="Email"
+          type="email"
+         
+          onChange={(e)=>setEmail(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <EmailOutlinedIcon style={{ marginRight: "8px" }} id="lock" />
+            ),
+          }}
+        />
+        <small>Error Message</small>
 
-   <>
-   <body id='body'>
-    
-    <Typography >
-      <h2 id='h2-sign'>Signup</h2>
-      <div id='underline'></div>
-    </Typography>
-      <TextField
-        id='mail-log'
-        label='Email' 
-        type='email '
-        InputProps={{
-          startAdornment: (
-            <EmailOutlinedIcon style={{ marginRight: '8px' }} id='lock' />
-          ),
-        }}
-      />
+        <TextField
+          id="pass-log"
+          label="Password"
+          type="password"
+          onChange={(e)=>setPassword(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <LockOutlinedIcon style={{ marginRight: "8px" }} id="lock" />
+            ),
+          }}
+        />
 
-      <TextField 
-        id='pass-log'
-        label='Password' 
-        type='password'
-        InputProps={{
-          startAdornment: (
-            <LockOutlinedIcon style={{ marginRight: '8px' }} id='lock' />
-          ),
-        }}
-     
-      />
-
-      <TextField 
-        id='pass-log'
-        label='Confirm Password' 
-        type='password'
-        InputProps={{
-          startAdornment: (
-            <LockOutlinedIcon style={{ marginRight: '8px' }} id='lock' />
-          ),
-        }}
-     
-      />
-      <Button id='btn-signin' variant='contained'>
-       Signup
-       
-      </Button>
-      <div id='aup-log'>
-       <label id='al-sign'>
-        Already have an account?  
-       </label> 
-       <Link id='sign-log' to='/'>
-         &nbsp; Login
-       </Link>
-       </div>
-
-  </body>
+        <TextField
+          id="conpass-log"
+          label="Confirm Password"
+          type="password"
+          onChange={(e)=>setConfirmPassword(e.target.value)}
+          InputProps={{
+            startAdornment: (
+              <LockOutlinedIcon style={{ marginRight: "8px" }} id="lock" />
+            ),
+          }}
+        />
+        <Button id="btn-signin" onClick={createUser} variant="contained">
+          Signup
+        </Button>
+        <div id="aup-log">
+          <label id="al-sign">Already have an account?</label>
+          <Link id="sign-log" to="/">
+            &nbsp; Login
+          </Link>
+        </div>
+      </div>
     </>
-     
   );
-}
+};
 
-
-export default Signup
-
+export default Signup;
