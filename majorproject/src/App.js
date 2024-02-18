6bf3fae0884477221724a2259752d77240163b85
Siva-0107 <ss9502830@gmail.com>
2024-02-18 18:47:38 +0530

    gogo

--- majorproject/src/App.js
+++ majorproject/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {Login} from './Component/Assets/Login/Login';
 import Signup from './Component/Assets/Signup/Signup';
+import Dashboard from './Component/Assets/Dashboard/Dashboard';
 import { Route,Routes} from 'react-router-dom';
 function App() {
   return (
@@ -9,7 +10,11 @@ function App() {
     <Routes>
       <Route path='/' Component={Login}  />
      < Route path='/Signup' Component={Signup} />
+     <Route  path='/Dashboard' Component={Dashboard} />
     </Routes>
+  
+  
+
    </>
   );
 }
