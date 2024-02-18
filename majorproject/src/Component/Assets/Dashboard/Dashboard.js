18f6624786e9a4b8e4785fd2f8d6e739b51a6f8e
Siva-0107 <ss9502830@gmail.com>
2024-02-18 23:21:06 +0530

    gogo

--- majorproject/src/Component/Assets/Dashboard/Dashboard.js
+++ majorproject/src/Component/Assets/Dashboard/Dashboard.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import './Dashboard.css'
+import {BiExport,BiMailSend } from 'react-icons/bi'
+import {FaHistory, FaTasks} from "react-icons/fa"
+import { MdOutlineCreate, MdOutlineTranslate, MdOutlineDashboard, MdOutlineLogout } from "react-icons/md";
+import { RiAdvertisementLine } from "react-icons/ri";
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className='body'>
+      <div className='menu'>
+        <div className='logo'>
+         <MdOutlineDashboard className='logo-icon' />
+          <h2>Dashboard</h2>
+        </div>
+
+
+{/*  ---------------- SlideBar-Left Start----------------- */}
+
+        <div className='menu--list'>
+          <a href='#' className='item'>
+            <FaTasks  className='icon'/>
+            Task-Group
+          </a>
+          <a href='#' className='item'>
+            <BiMailSend className='icon'/>
+            Mail-Sender
+          </a>
+          <a href='#' className='item'>
+           <MdOutlineTranslate  className='icon'/>
+            Translate
+          </a>
+          <a href='#' className='item'>
+          <RiAdvertisementLine className='icon'/>
+            Advertisement
+          </a>
+          <a href='#' className='item'>
+            <MdOutlineCreate className='icon'/>
+            AI-Creater
+          </a>
+           <a href='#' className='item'>
+            <FaHistory className='icon'/>
+            History
+          </a>
+          <a href='#' className='item'>
+            <BiExport className='icon'/>
+            Export
+          </a>
+          <a href='#' className='item'>
+            <MdOutlineLogout className='icon'/>
+            Logout
+          </a>
+        </div>    
+
+{/*  ---------------- SlideBar-Left End----------------- */}
+
+
+      </div>
     </div>
   )
 }
