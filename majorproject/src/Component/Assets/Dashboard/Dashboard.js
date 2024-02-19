12f84865faeee2a1c030aab995a0a798e0b44b88
Siva-0107 <ss9502830@gmail.com>
2024-02-19 20:43:42 +0530

    bjk

---
+++ majorproject/src/Component/Assets/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from './Sidebar/Sidebar';
+import './Dashboard.css'
+ const Dashboard = () => {
+  return (
+    <div className='dashboard'>
+        <Sidebar />
+    </div>
+  )
+}
+export default Dashboard
---
+++ majorproject/src/Component/Assets/Dashboard/Sidebar/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '.\E:\React_JS\majorproject\src\Component\Assets\Dashboard\Sidebar\Sidebar.css'
+
+const Sidebar = () => {
+  return (
+    <div>Sidebar</div>
+  )
+}
+export default Sidebar
